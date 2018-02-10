47a5f3b982107ebf75a93b91e39606772720cd32
Cesca <csinocruz@gmail.com>
2018-02-09 16:40:07 -0800

    cards flip correctly when a match is found or not found, also added wind chime sound when matching

--- script.js
+++ script.js
@@ -24,6 +24,10 @@ var audio = new Audio('/Users/francescasinocruz/Desktop/LFZ/crystal-memory-match
 audio.loop = true;
 audio.play();
 
+// card match sound
+// var sparkle = new Audio('/Users/francescasinocruz/Desktop/LFZ/crystal-memory-match/assets/sounds/twinkle.mp3');
+// sparkle.volume = 0.2;
+
 var isPlaying = true;
 
 function togglePlay() {
@@ -42,10 +46,11 @@ function initializeApp() {
 };
 
 function clickHandler() {
+    console.log('user clicked on a card');
     var the_card = $(this);
-    $(the_card).addClass('spin');
-    window.setTimeout(function () {
-        $(the_card).toggleClass('reveal');
+    the_card.addClass('spin');
+    setTimeout(function () {
+        the_card.toggleClass('reveal');
     }, 500);
 
     // check whether it is first or second card clicked
@@ -53,18 +58,65 @@ function clickHandler() {
         first_card_clicked = $(this);
     } else {
         second_card_clicked = $(this);
-        attempts++;
 
         // check for match
         if (first_card_clicked.find('img').attr('src') === second_card_clicked.find('img').attr('src')) {
+            var sparkle = new Audio('/Users/francescasinocruz/Desktop/LFZ/crystal-memory-match/assets/sounds/twinkle.mp3');
+            sparkle.volume = 0.2;
+            sparkle.play();
+            setTimeout(function () {
+                sparkle.pause();
+            }, 4600);
+            console.log('we have matching cards');
+            setTimeout(function() {
+                first_card_clicked.css({
+                    'opacity': '0'
+                });
+                $(first_card_clicked).toggleClass('spin');
+                first_card_clicked = null;
+            }, 1100);
+            setTimeout(function() {
+                second_card_clicked.css({
+                    'opacity': '0'
+                });
+                $(second_card_clicked).toggleClass('spin');
+                second_card_clicked = null;
+            }, 1100);
             match_counter++;
             total_matches_counter++;
-            console.log('we have matching cards');
-            // REMOVE CARDS FROM BOARD
         } else {
-            // NO MATCH, FLIP CARDS BACK OVER
+            console.log('cards are not a match');
+            // handles for the first card
+            setTimeout(function() {
+                first_card_clicked.removeClass('spin');
+                can_click = true;
+            }, 1000);
+            setTimeout(function () {
+                first_card_clicked.addClass('spin');
+            }, 1500);
+            setTimeout(function () {
+                first_card_clicked.toggleClass('reveal');
+            }, 2000);
+            setTimeout(function () {
+                first_card_clicked.removeClass('spin');
+                first_card_clicked = null;
+            }, 2500);
+
+            // handles for the second card
+            setTimeout(function() {
+                second_card_clicked.removeClass('spin');
+                can_click = true;
+            }, 1000);
+            setTimeout(function () {
+                second_card_clicked.addClass('spin');
+            }, 1500);
+            setTimeout(function () {
+                second_card_clicked.toggleClass('reveal');
+            }, 2000);
+            setTimeout(function () {
+                second_card_clicked.removeClass('spin');
+                second_card_clicked = null;
+            }, 2500);
         }
     }
-
-
 }
